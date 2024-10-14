89612064164ad99cb917f7fb8e741a4cc7168a50
foysalahmedmin <foysalahmedmin@gmail.com>
2024-10-14 12:25:22 +0600

    user profile page

--- src/pages/(user)/UserProfilePage/index.jsx
+++ src/pages/(user)/UserProfilePage/index.jsx
@@ -1,5 +1,25 @@
+import ProfileSection from "@/components/(user)/profile/ProfileSection";
+import BannerSection from "@/components/partials/Sections/BannerSection";
+
 const UserProfilePage = () => {
-  return <main></main>;
+  return (
+    <main>
+      <BannerSection
+        title="Profile Settings"
+        breadcrumbItems={[
+          {
+            label: "Home",
+            path: "/",
+          },
+          {
+            label: "Profile Setting",
+            path: "/user/profile",
+          },
+        ]}
+      />
+      <ProfileSection />
+    </main>
+  );
 };
 
 export default UserProfilePage;
